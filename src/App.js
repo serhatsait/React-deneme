00c0656e051ce221eff5b34ab99eb368ede223be
serhatsait <serhatsaitpekediz@gmail.com>
2019-09-19 14:20:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import faker from 'faker';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="ui comments">
+            <div className="comment">
+                <a className="avatar" href='/'>
+                    <img alt={""} src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href='/' className="author">Tom Lukic</a>
+                    <div className="text">
+                        This will be great for business reports. I will definitely download this.
+                    </div>
+                    <div className="actions">
+                        <a href='/' className="reply">Reply</a>
+                        <a href='/' className="save">Save</a>
+                        <a href='/' className="hide">Hide</a>
+                        <a href='/'>
+                            <i className="expand icon"></i>
+                            Full-screen
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
